d204ea34778f157d9d87ed0770c3512097c88da6
Cezar <cezarlantosgmail.com>
2021-12-10 17:37:45 +0100

    esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
 const containerBoxes = document.querySelector('.container-boxes');
 
-
-for (let i = 1; i < 100; i++) {
-    if (i % 5 === 0 && i !== 3) {
-        containerBoxes.innerHTML += `<div class="box box-red">${FizzBuzz}<div>`
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        containerBoxes.innerHTML += `<div class="box box-red">FizzBuzz<div>`
     } 
-    else if (i % 5 === 0) {
-        containerBoxes.innerHTML += `<div class="box box-orange">${Buzz}<div>`
-    }
     else if (i % 3 === 0) {
-        containerBoxes.innerHTML += `<div class="box box-aqua">${Fizz}<div>`
+        containerBoxes.innerHTML += `<div class="box box-palegreen">Fizz<div>`
+    }
+    else if (i % 5 === 0) {
+        containerBoxes.innerHTML += `<div class="box box-orange">Buzz<div>`
     }
     else {
         containerBoxes.innerHTML += `<div class="box box-teal">${i}<div>`
     }
-}
-
-for (let i = 0; i < 16; i++) {
-    if (i % 5 === 0 && i !== 3) {
-        // divisibile per 5
-        containerBoxes.innerHTML += `<div class="box box-red">${i} (ciao)</div>`  
-    }
-    else if (i % 2 === 0) {
-        // pari
-        containerBoxes.innerHTML += `<div class="box">${i}</div>` 
-    }  
-    else {
-        // dispari
-        containerBoxes.innerHTML += `<div class="box box-teal">${i}</div>`
-    }
-
 }
